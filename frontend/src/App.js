fb6cf580abe03dc9a02fa12742a8e360a183e69f
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-04-03 13:26:17 +0530

    Error boundaries added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import AppRoutes from './AppRoutes/AppRoutes'
-
+import React from "react";
+import AppRoutes from "./AppRoutes/AppRoutes";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./Components/ErrorFallback/ErrorFallback";
 
 export default function App() {
   return (
     <>
-      <AppRoutes />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          window.location.reload();
+        }}>
+        <AppRoutes />
+      </ErrorBoundary>
     </>
-  )
+  );
 }
--- frontend/src/AppRoutes/AppRoutes.js
+++ frontend/src/AppRoutes/AppRoutes.js
@@ -3,28 +3,49 @@ import DefaultPage from "../Pages/DefaultPage/DefaultPage";
 import Login from "../Pages/Login/Login";
 import MyNotes from "../Pages/MyNotes/MyNotes";
 import SignUp from "../Pages/SignUp/SignUp";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import AuthRoute from "../Components/AuthRoute/AuthRoute";
 import Profile from "../Pages/Profile/Profile";
 
-
-
-
-
 export default function AppRoutes() {
+  if (1) {
+    throw new Error("test error boundary");
+  }
   return (
     <>
-      <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
+      <Toaster
+        position="top-center"
+        toastOptions={{ duration: 2000 }}
+      />
       <Routes>
-        <Route path="/" element={<DefaultPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/notes" element={<AuthRoute>
-          <MyNotes />
-        </AuthRoute>} />
-        <Route path="/profile" element={<AuthRoute>
-          <Profile />
-        </AuthRoute>} />
+        <Route
+          path="/"
+          element={<DefaultPage />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/signup"
+          element={<SignUp />}
+        />
+        <Route
+          path="/notes"
+          element={
+            <AuthRoute>
+              <MyNotes />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <AuthRoute>
+              <Profile />
+            </AuthRoute>
+          }
+        />
       </Routes>
     </>
   );
